da398473be28d4f2d23422f85645d7deebe091a4
Miguel Mota <miguelmota2@gmail.com>
2017-10-01 12:00:23 -0700

    clean up

--- src/generator.js
+++ src/generator.js
@@ -12,17 +12,18 @@ async function transform(podcastUrl) {
     const xml = await request(podcastUrl)
     const result = await pify(parseString)(xml)
     const { channel } = result.rss
-    const items = channel[0].item
+    const { item: items } = channel[0]
 
     const flashBriefingJson = items.map(item => {
       const guid = item.guid[0]._
-      const link = item.link[0]
-      const audioUrl = item.enclosure[0]['$'].url
+      const id = `urn:guid:${guid}`
       const date = moment(item.pubDate[0], 'ddd, DD MMM HH:mm:ss').format(`YYYY-MM-DD[T]HH:mm:ss[.0Z]`)
       const title = item.title[0]
+      const audioUrl = item.enclosure[0]['$'].url
+      const link = item.link[0]
 
       return {
-        uid: `urn:guid:${guid}`,
+        uid: id,
         updateDate: date,
         titleText: title,
         mainText: '',
